504a93df5e7a1aba6da7ffb76309d439d2138b76
Ranielli Montagna <raniellimontagna@hotmail.com>
2022-12-31 17:13:45 -0300

    refactor: implementa estilização da lista de transações

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de website</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>31/12/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">-R$ 32,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>31/12/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
